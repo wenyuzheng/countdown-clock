0a941f155dfcdfcec5b5f4963b4c4cb5540e4e8b
Wenyu Zheng <zhengwy229@gmail.com>
2023-07-15 17:37:31 +0100

    able to countdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const timer = 61;
+  const [minute, setMinute] = useState(Math.floor(timer / 60));
+  const [second, setSecond] = useState(timer % 60);
+  const [start, setStart] = useState(false);
+
+  const handleStartClick = () => {
+    setStart(true);
+  };
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      if (start) {
+        if (minute === 0 && second === 0) {
+          clearInterval(countdown);
+          setStart(false);
+          return;
+        }
+        const newSec = second - 1;
+
+        if (newSec < 0) {
+          setSecond(59);
+          setMinute(minute - 1);
+        } else {
+          setSecond(newSec);
+        }
+      } else {
+        clearInterval(countdown);
+        setStart(false);
+      }
+    }, 1000);
+
+    return () => clearInterval(countdown);
+  }, [start, minute, second]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleStartClick}>Start</button>
+      <div>
+        {minute}:{second}
+      </div>
     </div>
   );
 }
